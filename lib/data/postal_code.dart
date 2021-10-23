770197a811e64ea3591ca684d40edb18cf2bc07d
sakushin <sakushin.udemy@gmail.com>
2021-10-24 06:48:33 +0900

    PostalCodeをテストした

--- lib/data/postal_code.dart
+++ lib/data/postal_code.dart
@@ -15,11 +15,28 @@ class PostalCode with _$PostalCode {
       _$PostalCodeFromJson(json);
 }
 
+@freezed
 class PostalCodeData with _$PostalCodeData {
   const factory PostalCodeData({
-    required String code,
+    required String prefcode,
+    required PostalCodeAddress ja,
+    required PostalCodeAddress en,
   }) = _PostalCodeData;
 
   factory PostalCodeData.fromJson(Map<String, dynamic> json) =>
       _$PostalCodeDataFromJson(json);
+}
+
+@freezed
+class PostalCodeAddress with _$PostalCodeAddress {
+  const factory PostalCodeAddress({
+    required String prefecture,
+    required String address1,
+    required String address2,
+    required String address3,
+    required String address4,
+  }) = _PostalCodeAddress;
+
+  factory PostalCodeAddress.fromJson(Map<String, dynamic> json) =>
+      _$PostalCodeAddressFromJson(json);
 }
