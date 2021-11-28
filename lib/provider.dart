ef6b0054e152e24956fdf37c6207b829b11eff62
sakushin <sakushin.udemy@gmail.com>
2021-11-28 12:59:29 +0900

    autoDispose, familyを追加

--- lib/provider.dart
+++ lib/provider.dart
@@ -30,3 +30,29 @@ FutureProvider<PostalCode> apiProvider = FutureProvider((ref) async {
   var jsonData = json.decode(response.body);
   return PostalCode.fromJson(jsonData);
 });
+
+//FutureProviderFamily<PostalCode, String> apiFamilyProvider =
+AutoDisposeFutureProviderFamily<PostalCode, String> apiFamilyProvider =
+    FutureProvider.autoDispose
+        .family<PostalCode, String>((ref, postalcode) async {
+  if (postalcode.length != 7) {
+    throw Exception("Postal Code must be 7 charactors");
+  }
+
+  // 123-4567
+  final upper = postalcode.substring(0, 3); //123
+  final lower = postalcode.substring(3); // 4567
+
+  //https://madefor.github.io/postal-code-api/api/v1/100/0014.json
+  final apiUrl =
+      'https://madefor.github.io/postal-code-api/api/v1/$upper/$lower.json';
+  final apiUri = Uri.parse(apiUrl);
+  http.Response response = await http.get(apiUri);
+
+  if (response.statusCode != 200) {
+    throw Exception('No postal code: $postalcode');
+  }
+
+  var jsonData = json.decode(response.body);
+  return PostalCode.fromJson(jsonData);
+});
